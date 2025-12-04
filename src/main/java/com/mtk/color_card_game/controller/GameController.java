37164086685c8f82ec880cc7058af690e0bf2643
Myat Thukha <thukhamyat2000@gmail.com>
2025-12-04 10:57:13 +0630

    add controller

---
+++ src/main/java/com/mtk/color_card_game/controller/GameController.java
@@ -0,0 +1,26 @@
+package com.mtk.color_card_game.controller;
+
+import com.mtk.color_card_game.dto.GameRequest;
+import com.mtk.color_card_game.dto.GameResponse;
+import com.mtk.color_card_game.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/play")
+public class GameController {
+
+    private final GameService service;
+
+    @PostMapping
+    public ResponseEntity<?> handlePlayGame(@RequestBody GameRequest request) {
+        GameResponse response = service.playGame(request);
+
+        return ResponseEntity.ok(response);
+    }
+}

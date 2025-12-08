ac09f4470ed2a0cb86e98103150a07208577da34
Myat Thukha <thukhamyat2000@gmail.com>
2025-12-08 15:48:07 +0630

    add carry over scheduler

--- src/main/java/com/mtk/color_card_game/ColorCardGameApplication.java
+++ src/main/java/com/mtk/color_card_game/ColorCardGameApplication.java
@@ -2,8 +2,10 @@ package com.mtk.color_card_game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ColorCardGameApplication {
 
 	public static void main(String[] args) {

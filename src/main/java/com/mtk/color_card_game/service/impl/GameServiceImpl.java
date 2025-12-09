e17bf8974f3107b71e2a1f05369c7b32b887a727
Myat Thukha <thukhamyat2000@gmail.com>
2025-12-09 13:31:49 +0630

    add drop rate and change event day

--- src/main/java/com/mtk/color_card_game/service/impl/GameServiceImpl.java
+++ src/main/java/com/mtk/color_card_game/service/impl/GameServiceImpl.java
@@ -7,14 +7,16 @@ import com.mtk.color_card_game.entity.DailyPrize;
 import com.mtk.color_card_game.entity.Prize;
 import com.mtk.color_card_game.repo.DailyPrizeRepo;
 import com.mtk.color_card_game.repo.PrizeRepo;
-import com.mtk.color_card_game.repo.UserRepo;
 import com.mtk.color_card_game.service.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 @Slf4j
@@ -22,11 +24,17 @@ import java.util.*;
 @RequiredArgsConstructor
 public class GameServiceImpl implements GameService {
 
-    private final UserRepo userRepo;
+    @Value("${event.start.date}")
+    private String eventStartDateString;
+
+    @Value("${event.duration.days}")
+    private int eventDuration;
+
     private final PrizeRepo prizeRepo;
     private final DailyPrizeRepo dailyPrizeRepo;
 
     private final Random random = new Random();
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     private enum Color {
         ORANGE, GREEN, BLUE
@@ -49,7 +57,7 @@ public class GameServiceImpl implements GameService {
 
         if (availablePrizes.isEmpty()) {
             log.info("No Prizes For Today.");
-            throw new CommonException("ERR_404", "No prizes are available for today's game.");
+            throw new CommonException("ERR_404", "No prizes available for today.");
         }
 
         Color userColor;
@@ -132,22 +140,33 @@ public class GameServiceImpl implements GameService {
             throw new CommonException("ERR_500", "No Prize Left For Rank " + rank);
         }
 
-        return filtered.get(random.nextInt(filtered.size()));
+        int dropRate = filtered.stream().mapToInt(DailyPrize::getAvailableQuantity).sum();
+        int randomNumber = random.nextInt(dropRate) + 1;
+        int cumulative = 0;
+
+        for (DailyPrize prize : filtered) {
+            cumulative += prize.getAvailableQuantity();
+            if (randomNumber <= cumulative) {
+                return prize;
+            }
+        }
+        throw new RuntimeException("Random Number Generator Failed.");
     }
 
     private int getEventDay(LocalDate day) {
-        LocalDate day1 = LocalDate.of(2025, 12, 8);
-        LocalDate day2 = day1.plusDays(1);
-        LocalDate day3 = day2.plusDays(1);
+        LocalDate eventStartDate = LocalDate.parse(eventStartDateString, DATE_FORMATTER);
+        if (day.isBefore(eventStartDate)) {
+            return -1;
+        }
 
-        if (day.isEqual(day1))
-            return 1;
+        // 0
+        long daysBetween = ChronoUnit.DAYS.between(eventStartDate, day);
 
-        if (day.isEqual(day2))
-            return 2;
+        int currentEventDay = (int) daysBetween + 1;
 
-        if (day.isEqual(day3))
-            return 3;
+        if (currentEventDay >= 1 && currentEventDay <= eventDuration) {
+            return currentEventDay;
+        }
 
         return -1;
     }
